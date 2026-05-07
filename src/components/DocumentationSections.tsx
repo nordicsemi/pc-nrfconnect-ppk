6cb560dd987b9b61ae3cf399e5fc9072b341f8d7
Marko Schulz <Marko.Schulz@nordicsemi.no>
2026-05-07 11:34:16 +0200

    Update documentation links
    https://docs.nordicsemi.com/bundle/nrf-connect-ppk is the outdated
    bundle which is not updated anymore. Correct is now to link to
    https://docs.nordicsemi.com/bundle/swtools_docs.

--- src/components/DocumentationSections.tsx
+++ src/components/DocumentationSections.tsx
@@ -11,7 +11,7 @@ const DocumentationSections = [
     <DocumentationSection
         key="infocenter"
         linkLabel="Open documentation"
-        link="https://docs.nordicsemi.com/bundle/nrf-connect-ppk/page/index.html"
+        link="https://docs.nordicsemi.com/bundle/swtools_docs/page/app/pc-nrfconnect-ppk/index.html"
     >
         Visit TechDocs for official documentation of the app.
     </DocumentationSection>,
